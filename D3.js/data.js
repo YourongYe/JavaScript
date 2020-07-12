94965d4d67bc38ec7f125a18f404f8bc896a8c74
Yourong Ye <yourong.ye@hotmail.com>
2020-07-12 22:08:08 +0100

    Create data.js

---
+++ D3.js/data.js
@@ -0,0 +1,31 @@
+{
+   "name":"Astham",
+   "children":[
+      {
+         "name":"System-1",
+         "children":[
+            {
+               "name":"primary care",
+               "value":200
+            },
+            {
+               "name":"secondary care",
+               "value":300
+            }
+         ]
+      },
+      {
+         "name":"System-2",
+         "children":[
+            {
+               "name":"Meta data",
+               "value":400
+            },
+            {
+               "name":"Primary care",
+               "value":500
+            }
+         ]
+      }
+   ]
+}
