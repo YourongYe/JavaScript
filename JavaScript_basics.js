6c2cdcd2a7d2fd70d6d39377011c79bb26e0d8a3
Yourong Ye <yourong.ye@hotmail.com>
2020-07-14 22:31:00 +0100

    Create JavaScript_basics.js

---
+++ JavaScript_basics.js
@@ -0,0 +1,6 @@
+// what does ... mean? equal to * in python
+//1. used in function parameter
+function myFunc(...parameter) // 可能有多个参数
+//2. used in list, 把list里的所有element取出来单独放入另一个容器
+var l1 = [1,2,3]
+var l2 = [...l1, 4,5,6] // [1,2,3,4,5,6]
